f9ed4b42fe4f6f7e94586380460f89c5426fa436
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2023-06-17 17:49:42 +0800

    edited: index and vivi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,13 @@ import Navbar from "~/components/navbar/Navbar";
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
   const [message, setMessage] = useState('')
+  const [journalText, setJournalText] = useState('')
+  const [journalWrite, setJounralWrite] = useState(false)
   const [greeted, setGreeted ] = useState(false)
+
+  function saveJournal(textEntry: string) : void {
+    // create new journal entry and save to database
+  }
 
   useEffect(() => {
     console.log(sessionData)
@@ -30,9 +36,17 @@ const Home: NextPage = () => {
         <div className="h-screen w-screen bg-gray-900 absolute top-0">
           <Navbar currentPage="home" />
           <div className="h-1/4 w-3/5 relative top-28 left-48">
-            <h1 className="text-white text-6xl font-bold"> {message} </h1>
+            <h1 className="text-white text-5xl font-bold"> {message} </h1>
           </div>
-          <Vivi message={message} greeted={greeted} setGreeted={setGreeted}/>
+          <div className="h-1/4 w-3/5 relative top-28 left-48">
+            <h1 className="text-white text-3xl"> {journalText} </h1>
+          </div>
+          {journalText.length > 0 &&
+            <div className="h-1/4 w-3/5 relative top-28 left-48">
+              <button className="absolute bottom-10 h-32 w-32 rounded-lg bg-white p-0 text-3xl" onClick={() => saveJournal(journalText)}> Save as Journal? </button>
+            </div>
+          }
+          <Vivi message={message} greeted={greeted} setGreeted={setGreeted} setJournalWrite={setJounralWrite} journalWrite={journalWrite} setJournalText={setJournalText} journalText={journalText}/>
         </div>
        :
        <div className="flex min-h-screen flex-col items-center justify-center bg-white">
