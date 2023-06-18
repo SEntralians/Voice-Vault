21ad6a37f2b0176b492a2f79a0c48753f3e3e443
unknown <ledesma.matthew.b@gmail.com>
2023-06-18 14:32:36 +0800

    update discussion content

--- src/components/forum/sections-topics-list.tsx
+++ src/components/forum/sections-topics-list.tsx
@@ -5,22 +5,25 @@ const Categories: React.FC = () => {
     <div className="flex flex-col bg-white px-4 py-6 max-w-sm mx-auto rounded-lg shadow-md">
       <ul className="">
         <li>
-          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- AWS</a>
+          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- Anime</a>
         </li>
         <li className="mt-2">
-          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- Laravel</a>
+          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- Debate</a>
         </li>
         <li className="mt-2">
-          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- Vue</a>
+          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- Funny</a>
         </li>
         <li className="mt-2">
-          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- Design</a>
+          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- Life-Crisis</a>
         </li>
         <li className="flex items-center mt-2">
-          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- Django</a>
+          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- Memes</a>
         </li>
         <li className="flex items-center mt-2">
-          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- PHP</a>
+          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">- Shower Thoughts</a>
+        </li>
+        <li className="flex items-center mt-2">
+          <a className="text-gray-700 font-bold mx-1 hover:text-gray-600 hover:underline" href="#">...</a>
         </li>
       </ul>
     </div>
